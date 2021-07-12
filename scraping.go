55d10d08f038f6489f916ef790db83a0f6eead8a
lanh.hung.son <lanh.hung.son@sun-asterisk.com>
2021-07-12 10:25:12 +0700

    scraping web

---
+++ scraping.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("vnexpress.net"),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://vnexpress.net/")
+}
+
